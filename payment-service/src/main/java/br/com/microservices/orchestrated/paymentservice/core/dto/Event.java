7ed269f842a6268dc0cf4141948ef0eb366105a9
Jonathan <jonathanb.16@hotmail.com>
2026-06-25 11:34:11 -0300

    starting developing the payment logic

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
@@ -17,7 +17,7 @@ public class Event {
 
 
     private String id;
-    private String transactionalId;
+    private String transactionId;
     private String orderId;
     private Order payload;
     private String source;
