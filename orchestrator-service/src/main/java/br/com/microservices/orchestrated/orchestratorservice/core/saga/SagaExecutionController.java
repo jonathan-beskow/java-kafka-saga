3da19417454c28a8aa209d655306369386bae402
M373746 <j.beskow@sistran.com.br>
2026-06-29 10:38:41 -0300

    ending implementation for orchestrator

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/saga/SagaExecutionController.java
@@ -0,0 +1,4 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.saga;
+
+public class SagaExecutionController {
+}
---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/service/OrchestratorService.java
@@ -0,0 +1,4 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.service;
+
+public class OrchestratorService {
+}
